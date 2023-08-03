2c810e7b963a8bf7e934d6ceee02e38a106b99f3
abbassmoussawi <abbass.y.moussawi@gmail.com>
2023-08-03 14:52:58 +0300

    designing the first page using angualr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'am-root',
+  templateUrl: './nav.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'e-commerce';
+  info={
+    Fname: 'abbass',
+    Lname:'Moussawi'
+  }
 }
